de9583a1f5be788cce159f10598809edad51f076
ZhafranVI <116346065+ZhafranVI@users.noreply.github.com>
2022-12-25 20:57:39 +0700

    Add files via upload

---
+++ TPMODUL3 ZHAFRAN/src/Restaurant.java
@@ -0,0 +1,51 @@
+public class Restaurant implements Runnable {
+
+    private boolean waitingForPickup = false;
+    private static final Object lock = new Object();
+    private static int coffeeNumber = 1;
+
+    @Override
+    public void run() {
+        while (true) {
+            makeFood();
+            try {
+                    Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setWaitingForPickup(boolean waitingForPickup) {
+        this.waitingForPickup = waitingForPickup;
+    }
+
+    public static int getFoodNumber() {
+        return coffeeNumber;
+    }
+
+    public void makeFood() {
+        synchronized(Restaurant.lock) {
+            // check if waitingForPickup is true so call method lock.wait() to lock for thread
+            // so process not to continue before waiter deliver coffee
+            if (this.waitingForPickup) {
+                try {
+                    System.out.println("Restaurant: Waiting for the Waiter to deliver the food");
+                    Restaurant.lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            // to inform another thread that another coffee is ready to deliver
+            waitingForPickup = true;
+            System.out.println("Restaurant:  Making Food Number " + coffeeNumber++);
+            Restaurant.lock.notifyAll();
+            System.out.println("Restaurant: Telling the waiter to get the food\n");
+        }
+    }
+
+    public static Object getLock() {
+        return lock;
+    }
+}
